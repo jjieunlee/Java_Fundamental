26c0ddc5a7db7b3a3e60edb5edb0e8aa7154e40b
jk04115 <jk04115@naver.com>
2020-05-20 17:33:17 +0900

    2020-05-20

--- src/java_20200520/step4/StopWatchDemo.java
+++ src/java_20200520/step4/StopWatchDemo.java
@@ -3,35 +3,17 @@ package java_20200520.step4;
 public class StopWatchDemo {
 
 	public static void run(StopMilliWatch s1) {
-		s1.start();
-		for (long i = 0; i < 2000_000_000l; i++) {
-
-		}
-		s1.stop();
-
-		double elapsedTime = s1.getElapsedTime();
-		System.out.printf("경과시간: %.3f", elapsedTime);
-		System.out.println();
+		s1.execute();
 	}
 
 	public static void run(StopNanoWatch s1) {
-		s1.start();
-		for (long i = 0; i < 2000_000_000l; i++) {
-
-		}
-		s1.stop();
-
-		double elapsedTime = s1.getElapsedTime();
-		System.out.printf("경과시간: %.9f", elapsedTime);
+		s1.execute();
 	}
 
 	public static void main(String[] args) {
-
+		// StopNanoWatch s1=new StopNanoWatch();
 		StopMilliWatch s1 = new StopMilliWatch();
 		run(s1);
-
-		StopNanoWatch s2 = new StopNanoWatch();
-		run(s2);
 
 	}
 
---
+++ src/java_20200520/step5/StopWatchDemo.java
@@ -0,0 +1,30 @@
+package java_20200520.step5;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class StopWatchDemo {
+
+	public static void run(StopWatch s1) {
+		s1.execute();
+	}
+
+	public static void main(String[] args) throws Exception {
+//		// StopNanoWatch s1=new StopNanoWatch();
+//		StopMilliWatch s1 = new StopMilliWatch();
+//		run(s1);
+		
+		//Java I0
+		BufferedReader br = new BufferedReader(
+				new FileReader("C:\\dev\\eclipse-workspace\\Java_Fundamental\\src\\java_20200520\\step5\\class.txt"));
+		String readLine = br.readLine();
+		
+		//Java Reflaction 
+		Class clazz=Class.forName(readLine);
+		StopWatch s=(StopWatch)clazz.newInstance();
+		run(s);
+		
+	}
+
+}
