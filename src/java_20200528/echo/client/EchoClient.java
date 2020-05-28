f65255f77f027f770efca4b4a113db058b723b7e
jk04115 <jk04115@naver.com>
2020-05-28 12:48:49 +0900

    2020-05-28

--- src/java_20200528/echo/client/EchoClient.java
+++ src/java_20200528/echo/client/EchoClient.java
@@ -1,6 +1,12 @@
 package java_20200528.echo.client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class EchoClient {
@@ -17,8 +23,28 @@ public class EchoClient {
 		try {
 			// 3. Socket 객체를 생성하여 서버와 연결을 시도한다.
 			socket = new Socket(ip, port);
-			System.out.println("서버와 연결 성공");
 
+			// 4. Socket 객체로 출력 스트림을 생성하여 서버로 메세지를 보낸다.
+			OutputStream out = socket.getOutputStream();
+			// Stream chain
+			OutputStreamWriter osw = new OutputStreamWriter(out);
+			// Stream chain
+			BufferedWriter bw = new BufferedWriter(osw);
+			bw.write("안녕하세요 저는 이지은 입니다.");
+			bw.newLine();
+			bw.flush();
+
+			//7. Socket객체로 입력 스트림을 생성하여 서버의 메세지를 읽는다.
+			InputStream in = socket.getInputStream();
+			//stream chain
+			InputStreamReader isr = new InputStreamReader(in);
+			BufferedReader br = new BufferedReader(isr);
+			String readLine = br.readLine();
+			
+			System.out.println("서버 메세지: "+readLine);
+			
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
